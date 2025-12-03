6b84a36aea5946d2130e4ee5c9bad3f8f6f1a351
Takayuki YAMADA <133923363+tyamada-biodataeng@users.noreply.github.com>
2025-12-03 19:29:18 +0900

    updated routing settings

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,9 +1,22 @@
 """
 URL configuration for config project.
 """
+from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('silk/', include('silk.urls', namespace='silk')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATICFILES_DIRS
+    )
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+    urlpatterns += debug_toolbar_urls()
