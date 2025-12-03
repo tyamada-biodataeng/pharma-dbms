6fa99c18eb0a78a9fdf126e8556b4c1c3d978e7f
Takayuki YAMADA <133923363+tyamada-biodataeng@users.noreply.github.com>
2025-12-03 19:45:47 +0900

    removed import of Group

--- app/accounts/admin.py
+++ app/accounts/admin.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
-from django.contrib.auth.models import Group
 
 from .models import CustomUser
 
