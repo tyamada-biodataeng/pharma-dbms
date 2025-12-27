e8046d3f8ed3b0dc10f9929a88c93c989d79414a
Takayuki YAMADA <133923363+tyamada-biodataeng@users.noreply.github.com>
2025-12-27 18:10:53 +0900

    created a view for login

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
+from accounts.forms import UserLoginForm
+
+
+class UserLoginView(LoginView):
+
+    form_class = UserLoginForm
+    template_name = 'accounts/login.html'
