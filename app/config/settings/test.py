43c37fe089c357e46261f6775509d012e7c8b86e
Takayuki YAMADA <133923363+tyamada-biodataeng@users.noreply.github.com>
2026-03-22 19:26:44 +0900

    fixed workflows for tests

--- app/config/settings/test.py
+++ app/config/settings/test.py
@@ -4,8 +4,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('DATABASE_NAME', default='postgres'),
-        'USER': env('POSTGRES_USER'),
-        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'USER': env('POSTGRES_USER', default='postgres'),
+        'PASSWORD': env('POSTGRES_PASSWORD', default='postgres'),
         'HOST': env('DATABASE_HOST', default='postgres'),
         'PORT': env('DATABASE_PORT', default='5432'),
     }
