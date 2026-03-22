d9010ad61c8a63a2162c8517efafb1bdd54b7ba2
Takayuki YAMADA <133923363+tyamada-biodataeng@users.noreply.github.com>
2026-03-22 19:20:56 +0900

    updated user and schema for tests

--- app/config/settings/test.py
+++ app/config/settings/test.py
@@ -4,10 +4,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('DATABASE_NAME', default='postgres'),
-        'USER': f'{env("DATABASE_USER")}_test',
-        'PASSWORD': env('DATABASE_PASSWORD'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
         'HOST': env('DATABASE_HOST', default='postgres'),
         'PORT': env('DATABASE_PORT', default='5432'),
-        'OPTIONS': {'options': f'-c search_path=test_{env("DATABASE_SCHEMA")},public'},
     }
 }
