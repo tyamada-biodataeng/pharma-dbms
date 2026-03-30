bf221ea4fb0189658c7585f658cc7325d2ae8372
Takayuki YAMADA <133923363+tyamada-biodataeng@users.noreply.github.com>
2026-03-30 19:41:10 +0900

    added soft delete methods

--- app/core/models.py
+++ app/core/models.py
@@ -1,19 +1,47 @@
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django_extensions.db.fields import CreationDateTimeField, ModificationDateTimeField
 
 
+class SoftDeleteQuerySet(models.QuerySet):
+
+    def soft_delete(self):
+        return self.update(deleted_at=timezone.now())
+
+    def alive(self):
+        return self.filter(deleted_at__isnull=True)
+
+
+class SoftDeleteManager(models.Manager):
+
+    def get_queryset(self):
+        return SoftDeleteQuerySet(self.model, using=self._db).alive()
+
+    def with_deleted(self):
+        return SoftDeleteQuerySet(self.model, using=self._db)
+
+
 class CustomBaseModel(models.Model):
-    created_at = CreationDateTimeField(_('created at'))
-    updated_at = ModificationDateTimeField(_('updated at'))
+    objects = SoftDeleteManager()
+    created_at = CreationDateTimeField(_("created at"))
+    updated_at = ModificationDateTimeField(_("updated at"))
     deleted_at = models.DateTimeField(null=True, blank=True, default=None)
 
     def save(self, **kwargs):
         self.update_modified = kwargs.pop(
-            'update_modified', getattr(self, 'update_modified', True)
+            "update_modified", getattr(self, "update_modified", True)
         )
         super().save(**kwargs)
 
     class Meta:
-        get_latest_by = 'updated_at'
+        get_latest_by = "updated_at"
         abstract = True
+
+    def soft_delete(self):
+        self.deleted_at = timezone.now()
+        self.save(update_fields=["deleted_at", "updated_at"])
+
+    def restore(self):
+        self.deleted_at = None
+        self.save(update_fields=["deleted_at", "updated_at"])
