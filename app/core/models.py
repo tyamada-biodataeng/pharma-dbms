68a828628b916c53d4206d1200fb64f895492620
Takayuki YAMADA <133923363+tyamada-biodataeng@users.noreply.github.com>
2026-03-30 19:22:11 +0900

    created the CustomBaseModel

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_extensions.db.fields import CreationDateTimeField, ModificationDateTimeField
 
-# Create your models here.
+
+class CustomBaseModel(models.Model):
+    created_at = CreationDateTimeField(_('created at'))
+    updated_at = ModificationDateTimeField(_('updated at'))
+    deleted_at = models.DateTimeField(null=True, blank=True, default=None)
+
+    def save(self, **kwargs):
+        self.update_modified = kwargs.pop(
+            'update_modified', getattr(self, 'update_modified', True)
+        )
+        super().save(**kwargs)
+
+    class Meta:
+        get_latest_by = 'updated_at'
+        abstract = True
