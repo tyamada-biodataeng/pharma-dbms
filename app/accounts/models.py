4469189018d831db9ba271e06f38be535380d1c4
Takayuki YAMADA <133923363+tyamada-biodataeng@users.noreply.github.com>
2026-03-31 08:46:39 +0900

    added several fields to the custom user model

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,33 +1,26 @@
-import uuid
-
+import uuid6
 from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser, PermissionsMixin
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
 )
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
 class CustomUserManager(BaseUserManager):
-
     def create_user(self, username, email, password=None):
 
         if not username:
             raise ValueError('Users must have a username.')
 
-        user = self.model(
-            username=username,
-            email=self.normalize_email(email)
-        )
+        user = self.model(username=username, email=self.normalize_email(email))
         user.set_password(password)
         user.save(using=self._db)
         return user
 
     def create_superuser(self, username, email, password=None):
-        user = self.create_user(
-            username=username,
-            email=self.normalize_email(email),
-            password=password
-        )
+        user = self.create_user(username=username, email=self.normalize_email(email), password=password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
@@ -35,29 +28,14 @@ class CustomUserManager(BaseUserManager):
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    id = models.UUIDField(
-        default=uuid.uuid4,
-        primary_key=True,
-        editable=False
-    )
-    username = models.CharField(
-        verbose_name=_('username'),
-        max_length=20,
-        unique=True
-    )
-    email = models.EmailField(
-        verbose_name=_('email address'),
-        max_length=254,
-        blank=True
-    )
-    is_active = models.BooleanField(
-        verbose_name=_('active'),
-        default=True
-    )
-    is_staff = models.BooleanField(
-        verbose_name=_('staff'),
-        default=False
-    )
+    id = models.UUIDField(default=uuid6.uuid7, primary_key=True, editable=False)
+    username = models.CharField(verbose_name=_('username'), max_length=20, unique=True)
+    email = models.EmailField(verbose_name=_('email address'), max_length=254, blank=True)
+    is_active = models.BooleanField(verbose_name=_('active'), default=True)
+    is_staff = models.BooleanField(verbose_name=_('staff'), default=False)
+    created_at = models.DateTimeField(verbose_name=_('created at'), auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name=_('updated at'), auto_now=True)
+    deleted_at = models.DateTimeField(verbose_name=_('deleted at'), null=True, blank=True, default=None)
 
     objects = CustomUserManager()
 
